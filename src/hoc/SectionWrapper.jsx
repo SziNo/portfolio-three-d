81bf8ff099c66b9827d566dc8bcaa4f08bf23e9c
SziNo <szino1848@gmail.com>
2023-05-04 23:35:04 +0200

    fix: projects content triggers now on mobile

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -8,9 +8,9 @@ const StarWrapper = (Component, idName) =>
     return (
       <motion.section
         variants={staggerContainer()}
-        initial={`${idName === 'projects' ? 'show' : 'hidden'}`}
+        initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: idName === 'projects' ? 0.1 : 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
