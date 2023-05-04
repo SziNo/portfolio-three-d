4f83a2f3f9b5ac86000704a42d7fbd68459ded9c
SziNo <szino1848@gmail.com>
2023-05-04 16:53:20 +0200

    add 3d portfolio project

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import backend from './backend.png'
+import creator from './creator.png'
+import mobile from './mobile.png'
+import web from './web.png'
+import github from './github.png'
+import menu from './menu.svg'
+import close from './close.svg'
+
+import css from './tech/css.png'
+import git from './tech/git.png'
+import html from './tech/html.png'
+import javascript from './tech/javascript.png'
+import mongodb from './tech/mongodb.png'
+import nodejs from './tech/nodejs.png'
+import reactjs from './tech/reactjs.png'
+import next from './tech/next.png'
+import redux from './tech/redux.png'
+import tailwind from './tech/tailwind.png'
+import typescript from './tech/typescript.png'
+import threejs from './tech/threejs.svg'
+
+import owl from './company/owl.png'
+import kulkertech from './company/kulkertech.png'
+import dh from './company/dh.jpg'
+
+import cover from './cover.jpg'
+
+export {
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  next,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  dh,
+  kulkertech,
+  owl,
+  cover,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,292 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  next,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  kulkertech,
+  owl,
+  dh,
+  cover,
+  threejs,
+} from '../assets'
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'experience',
+    title: 'Experience',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+]
+
+const services = [
+  {
+    title: 'Web Development',
+    icon: web,
+  },
+  {
+    title: 'Responsive Design',
+    icon: mobile,
+  },
+  {
+    title: 'JavaScript Frameworks',
+    icon: backend,
+  },
+  {
+    title: 'Version Control',
+    icon: creator,
+  },
+]
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'next',
+    icon: next,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+]
+
+const experiences = [
+  {
+    title: 'Student',
+    company_name: 'BZSH Foreign Trade Technical School',
+    icon: kulkertech,
+    iconBg: '#E6DEDD',
+    date: 'Sep 2021 - May 2022',
+    points: [
+      'This was the turning point where I made a firm decision and took my first steps towards pursuing a career as a developer.',
+      'During the first half of the term, we studied Python, C#, SQL and worked with Android Studio. We also learned HTML and CSS and created presentations using PowerPoint.',
+      'In the second half of the term, our focus shifted to ASP.NET, JavaScript and Angular. We formed teams of 2 or 3 people and worked on a RESTful project.',
+      'The term concluded with a test, a programming exam, and the presentation of our Node, Express, Mongoose, MongoDB, EJS, Bootstrap project.',
+    ],
+  },
+  {
+    title: 'Student',
+    company_name: 'EPAM Systems / Codecool',
+    icon: owl,
+    iconBg: '#E6DEDD',
+    date: 'May 2022 - Nov 2022',
+    points: [
+      'During the EPAM Junior Frontend Program, I was introduced to the basics of frontend development and important topics in JavaScript, with a focus on theoretical concepts.',
+      'The program also emphasized the importance of data structures and algorithms (DSA), a topic I had previously overlooked. While I spent a lot of time studying these topics, I also began learning React on Udemy.',
+      'After successfully completing the EPAM Program, I was accepted into the Career Starter Kit Program at Codecool.',
+      'During this period, I shifted my focus from theory to practice and began building React applications for my first portfolio. This ultimately led to landing my first job in tech.',
+    ],
+  },
+  {
+    title: 'Junior Frontend Software Engineer',
+    company_name: 'Docler Holding (ByBorg), Livejasmin Team',
+    icon: dh,
+    iconBg: '#E6DEDD',
+    date: 'Nov 2022 - Apr 2023',
+    points: [
+      'Developing and debugging mainly using React, Backbone.js, SASS, and other related technologies.',
+      'Collaborating with cross-functional teams including Designers, Web Content Translators, GA Specialists and Testers.',
+      'Implementing responsive design and ensuring cross-browser compatibility, usually using the Xcode simulator for iOS testing.',
+      'Participating in code reviews and getting acquainted with JIRA/BitBucket/Confluence.',
+    ],
+  },
+]
+
+const projects = [
+  {
+    name: 'Helpdesk App',
+    description:
+      'A user-friendly full-stack Helpdesk web application for managing Apple product support tickets. Features include user registration/login, ticket creation, note addition and ticket closure.',
+    tags: [
+      {
+        name: 'mern',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongoose',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'redux-toolkit',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'jwt',
+        color: 'orange-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/helpdesk-mern-app',
+  },
+  {
+    name: 'Shirtify',
+    description:
+      'Shirtify is a t-shirt designer app that lets you create custom designs with ease. With its 3D implementation using React and Three.js, you can upload images, choose colors and apply your design as a logo or to the entire shirt.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'three',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'framer-motion',
+        color: 'orange-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/Shirtify/tree/master/client',
+  },
+  {
+    name: 'Sumz',
+    description:
+      'This project utilizes a GPT-4 API to automatically generate concise and informative summaries of webpages from their URLs.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'rtk-query',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/gpt-4-summarizer',
+  },
+  {
+    name: 'Metaverse',
+    description:
+      'This project, similar to my hoobank project, focuses on aesthetics rather than functionality, with an emphasis on a fancy and neat appearance.',
+    tags: [
+      {
+        name: 'next',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'framer-motion',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/metaverse-next13',
+  },
+  {
+    name: 'Real Estate App',
+    description:
+      'This project allows users to sign in with Google authentication, update profile, post/manage advertisements, integrated Leaflet map, contact Landlord and password reminder.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'firebase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/realEstateAppWithFirebase',
+  },
+  {
+    name: 'HooBank',
+    description:
+      'By utilizing design tools such as Figma, the focus is on creating a seamless and intuitive user experience rather than on implementing complex functionalities.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'figma',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cover,
+    source_code_link: 'https://github.com/SziNo/hoobank',
+  },
+]
+
+export { services, technologies, experiences, projects }
