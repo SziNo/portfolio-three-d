fa8c2c8c6efa23066e01c2c88e7ac47dd27afbfd
SziNo <szino1848@gmail.com>
2023-05-06 16:55:17 +0200

    fix: add max-width to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,21 +12,23 @@ const Footer = () => {
       viewport={{ once: true, amount: 0 }}
       className={`${styles.paddingX} py-8 relative`}
     >
-      <div className='flex items-center justify-between flex-wrap'>
-        <h4 className='font-extrabold text-[24px] text-white'>Portfolio</h4>
-        <ul className='flex gap-6 list-none'>
-          {socials.map((social, idx) => (
-            <li key={idx}>
-              <img
-                key={idx}
-                src={social.image}
-                alt={social.name}
-                onClick={() => window.open(social.url, '_blank')}
-                className='w-[35px] h-[35px] object-contain cursor-pointer'
-              />
-            </li>
-          ))}
-        </ul>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex items-center justify-between flex-wrap'>
+          <h4 className='font-extrabold text-[24px] text-white'>Portfolio</h4>
+          <ul className='flex gap-6 list-none'>
+            {socials.map((social, idx) => (
+              <li key={idx}>
+                <img
+                  key={idx}
+                  src={social.image}
+                  alt={social.name}
+                  onClick={() => window.open(social.url, '_blank')}
+                  className='w-[35px] h-[35px] object-contain cursor-pointer'
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </motion.footer>
   )
